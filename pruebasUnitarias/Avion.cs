e15694ae9c2028db42d226e5919aea12880ba573
rodenas16616 <viki.rodenas@gmail.com>
2022-06-25 13:06:57 +0200

    anyado clase avion

---
+++ pruebasUnitarias/Avion.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+public class avion
+
+{
+	private float ALTURA;
+	private float v; 
+	private float combustible; private int o;
+	private void consumir_fuel(float litros)
+
+	public avion(float ALTURA, float
+	vel, float combustible, int o)
+	{
+		this.ALTURA = ALTURA; this.v = vel;
+		this.combustible = combustible; this.o = o;
+	}
+
+	public float Altura
+	{
+		get { return ALTURA; }
+	}
+
+	public int Orientacion
+	{
+		get
+		{
+			return
+				o;
+		}
+	}
+	public void Virar(int
+	  grados)
+	{
+		o = (o +
+			grados) % 360; consumir_fuel(grados * 0.1);
+	}
+
+	public float Combustible
+	{ get { return combustible; } }
+	//Metodos que sirve para ascender los metros indicados
+	public void a(float M) //M son los metros
+	{
+		ALTURA = ALTURA + M; consumir_fuel(M * 0.3);
+	}
+	//Metodos que sirve para descender los metros indicados
+	public void m(float
+						  metros)
+	{
+		ALTURA = ALTURA - metros;
+		if (ALTURA < 0)
+			ALTURA = 0;
+	}
+
+	{
+		combustible = combustible - litros;
+		if (combustible < 0)
+			combustible = 0;
+	}
+
+}
